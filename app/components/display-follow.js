f04c2c67a95eb78dbe3e881f031ac8565902a897
Ian Baum <ian@ianbaum.com>
2016-12-22 17:53:05 -0500

    add service

--- app/components/display-follow.js
+++ app/components/display-follow.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   followed: Ember.inject.service(),
-
-  actions: {
-    followQuestion(item){
-      this.get('followed').add(item);
-    },
-  }
 });
